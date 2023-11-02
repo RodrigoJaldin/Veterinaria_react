7978e65c081af364a55d11a4c200ae48fa3823ce
RodrigoJaldin <85416473+RodrigoJaldin@users.noreply.github.com>
2023-11-02 15:08:41 -0400

    Cambio header

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -75,7 +75,7 @@ const Formulario = ({ pacientes, setPacientes, paciente,setPaciente }) => {
 
       <form
         onSubmit={handleSubmit}
-        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 ">
 
         {
           error && <Error> <p>Todos los campos son obligatorios </p></Error>
